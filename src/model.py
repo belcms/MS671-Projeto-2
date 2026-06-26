8a0e9fe07fdd3d2f32ceb81487306de7e3894e7d
Isabel Cristina <isabelmarras13@gmail.com>
2026-06-25 23:41:55 -0300

    implementação da unidade de atenção

--- src/model.py
+++ src/model.py
@@ -37,3 +37,33 @@ X_entrada = Input(shape=(max_sequence_length,)) #ver da dimensionalidade
 x_emb = Embedding(input_dim=human_vocab_size, output_dim=embedding_dim, input_length=max_sequence_length)(X_entrada)
 
 a = Bidirectional(LSTM(units=lstm_units, return_sequences= True))(x_emb)
+
+
+repetidor = RepeatVector(max_sequence_length)
+concatenador = Concatenate(axis=-1)
+densa_1 = Dense(10, activation="tanh") 
+densa_2 = Dense(1, activation="relu") 
+ativacao_softmax = Activation("softmax", name='attention_weights')
+calculo_contexto = Dot(axes=1)
+
+def um_passo_de_atencao(a, s_prev):
+    """
+    Executa um passo de atenção para calcular o vetor de contexto.
+    
+    a: Saídas escondidas da Bi-LSTM (codificador)
+    s_prev: Estado escondido anterior da LSTM (decodificador)
+    """
+
+    s_prev_repetido = repetidor(s_prev)
+    
+    concatenado = concatenador([a, s_prev_repetido])
+    
+    e = densa_1(concatenado)
+    energias = densa_2(e)
+    
+
+    alfas = ativacao_softmax(energias)
+    
+    contexto = calculo_contexto([alfas, a])
+    
+    return contexto
